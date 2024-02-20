181b1c3a04cadc3595ba058f20c26594160a1781
sannasichottu <sannasichottu@gmail.com>
2024-02-20 15:14:52 +0530

    mern-login

---
+++ src/components/Login.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "../App.css";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  axios.defaults.withCredentials = true;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/auth/login", {
+        email,
+        password,
+      })
+      .then((response) => {
+        if (response.data.status) {
+          navigate("/home");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="sign-up-container">
+      <form className="sign-up-form" onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          autoComplete="off"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          placeholder="******"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Login</button>
+        <p>
+          Don't Have Account? <Link to="/signup">Sign Up</Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
